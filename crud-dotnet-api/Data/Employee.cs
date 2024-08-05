6bc054bd489510b36b01facafff369edf472a779
dheeraj.krishna <dheeraj.krishna@datalinksoftware.com>
2024-08-05 18:22:15 +0530

    Add project files.

---
+++ crud-dotnet-api/Data/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace crud_dotnet_api.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Salary { get; set; }
+        public string? Designation { get; set; }
+        public int MgrId { get; set; }
+
+    }
+}
---
+++ crud-dotnet-api/Migrations/20240718141306_Initial Setup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crud_dotnet_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MgrId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
